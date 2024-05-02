9122de64150b14e382ea4c91db1916f7f8ce12f1
redd223CJ <cordier.jordan2003@outlook.com>
2024-05-02 20:24:30 +0200

    Fibonacci

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+static unsigned long fibo(unsigned long n);
+
 int main()
 {
     printf("Hello World\n");
@@ -8,5 +10,23 @@ int main()
     
     // another test in other branch commit
 
+    unsigned long n = 15;
+    printf("fibo of %lu is %lu\n", n, fibo(n));
+
     return 0;
+}
+
+
+static unsigned long fibo(unsigned long n)
+{
+    if(n <= 1)
+        return n;
+    unsigned long a=0, b=1, temp;
+    for(long i = 0; i < n-1; i++)
+    {
+        temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 }
